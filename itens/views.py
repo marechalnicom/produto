a58752500438ceebbce9beb407226b2b642ef2b9
nicomedes <marechalnicom@gmail.com>
2023-06-14 08:57:01 -0300

    limpeza e adicionar tag

--- itens/views.py
+++ itens/views.py
@@ -1,25 +1,23 @@
 #from django.shortcuts import render
-from typing import Any, Dict, List
-from django import http
+from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.http import HttpRequest, HttpResponse
-from django.template import loader
-from django.http.response import JsonResponse
 from itens.models import Item, ItemEmpresa, Empresa, Mercadologica, Tag
 from django.views import generic
 import json
 import datetime
 import pandas as pd
+from django.db.models import Count
 
 # Create your views here.
 def menu():
     menu = {
             'index' : 'Home',
-            'itensemp': 'Itens por Empresa',
+            'itensemp' : 'Itens por Empresa',
             'mercadologica' : 'Extr. Mercadol. 2020',
             'somapaes' : 'Soma pães prefeitura',
-            'empresa': 'Empresas',
-            'tag':'Tags',
+            'empresa' : 'Empresas',
+            'tag' : 'Tags',
         }
     return menu
 class HomeView(generic.TemplateView):
@@ -89,12 +87,19 @@ class MercacologicaView(generic.ListView):
     def consultaMercadologica(self, obj):
         listamerc = []
         for ob in obj:
-            m = Mercadologica.objects.filter(codigo=ob['cod'], descricao=ob['desc']).values()
-            if m:
-                for query in m:
-                    ob['ID'] = f"ID: {query['id']}"
-            #else:
-                #salvaMercadologica(ob)
+            #m = Mercadologica.objects.filter(codigo=ob['cod'], descricao=ob['desc']).all()
+            #if 'tag' in ob:
+            #    for tag in ob['tag'].items():
+            #        #print(f'linha 93 obj {tag} mtag{m[0].tag.all()}')
+            #        t = Tag.objects.filter(tipo=tag[0].capitalize(), tag=tag[1], detalhe='Mercadologica')
+            #        if t.exists():
+            #            #print (f"existe t {t}")
+            #            if t[0] not in m[0].tag.all():
+            #                #print (f"\nexiste mtag\n t{t.values()}\n m{m[0].tag.values()}\n ")
+            #                m[0].tag.add(t[0])
+            #        else:
+            #            #ntag = m[0].tag.create(tipo=tag[0].capitalize(), tag=tag[1], detalhe='Mercadologica')
+            #            print(f'nova tag criada e adicionada ')
             listamerc.append(ob)            
         return listamerc
     def organizaMercadologica(self, obj):
@@ -145,20 +150,20 @@ class MercacologicaView(generic.ListView):
         obj = [x for x in obj if self.filtro.lower() in x['desc'].lower()]
         return obj
     def get_queryset(self):
-        m = Mercadologica.objects.filter(descricao__contains=self.filtro).order_by('id').values()
+        m = Mercadologica.objects.filter(descricao__contains=self.filtro).order_by('id').all()
         if self.extrutura == "r" or self.extrutura == "g" or self.extrutura == "c":
-            ids=[x['id'] for x in m if 0 == x['superior']]      
+            ids=[x['id'] for x in m.values() if 0 == x['superior']]      
             if self.extrutura == "g" or self.extrutura == "c":
                 r = Mercadologica.objects.filter(superior=0).order_by('id').values('id')
                 rid = [x['id'] for x in r]
                 rid.append(0)
-                ids=[x['id'] for x in m if x['superior'] in rid]
+                ids=[x['id'] for x in m.values() if x['superior'] in rid]
                 if self.extrutura == "c":
                     g = Mercadologica.objects.filter(superior__in=rid).order_by('id').values('id')
                     rid = [x['id'] for x in g]
                     rid.append(0)
-                    ids=[x['id'] for x in m if x['superior'] in rid ]
-            m = Mercadologica.objects.filter(pk__in=ids).order_by('id').values()
+                    ids=[x['id'] for x in m.values() if x['superior'] in rid ]
+            m = Mercadologica.objects.filter(pk__in=ids).order_by('id').all()
         return m
     
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
@@ -187,9 +192,7 @@ class SomarPaesView(generic.TemplateView):
     template_name = 'itens/somapaes_list.html'
     def analizacodigo(self, greq):
         getrec = {}
-        int(greq['ncoletor'])
-        print('inteiro possivel')
-        if len(greq['ncoletor']) == 13 and greq['ncoletor'][0] == '2':
+        if len(greq['ncoletor']) == 13 and greq['ncoletor'][0] == '2' and greq['ncoletor'].isnumeric() :
             getrec['codigo'] = greq['ncoletor']
             getrec['local'] = greq['nlocal']
             getrec['data'] = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -206,47 +209,25 @@ class SomarPaesView(generic.TemplateView):
             self.analizacodigo(request.GET)
         try:
             arquivo = (lerArquivo('itens/templates/itens/codigos.txt'))
-            print ("l188 tem arquivo",type(arquivo))
-            if isinstance(arquivo, dict):
-                print("l190 uma linha")
-            else:
-                print('l192 tem linha')
+            if not isinstance(arquivo, dict):
                 arquivo = arquivo.split("\r\n")
                 arquivo.pop()
-                #print(f'lista arquivo {arquivo}')
-                #nvarquivo = []
-                #for x in arquivo:
-                #    if x == '':
-                #        #print (f'lista v do arq {x}')
-                #       pass
-                #    else:
-                #        #print (f'lista do arq {x}')
-                #        nvarquivo.append(x)
-                #    arquivo = n
                 arquivo = [json.loads(x) for x in arquivo if x != '']
-                #print(f'l206 arquivo {arquivo}')
             
         except:
             print('não tem arquivo')
         if 'atualiza' in request.GET:
             try:
                 dados = json.loads(request.GET['dados'])
-                print(f'l213 dados {dados}')
                 dados['peso'] = round(int(dados['peso'])/1000, 3)
-                #print(f'l215 dado com peso c {dados}')
                 arquivo[int(request.GET['atualiza'])] = dados
-                print(f'l217 arquivo {arquivo}')
                 d=""
-                print (f'l 219')
                 indi=0
                 for x in arquivo:
                     if indi!=0:
-                        print(f'l221 l{x} ')
                         d += "\n"
                     d += json.dumps(x)
-                    print(f'l223')
                     indi=indi+1
-                print(f'atualizando {d}')
                 atualizararquivo(d, 'w')
 
             except:
@@ -265,7 +246,7 @@ class SomarPaesView(generic.TemplateView):
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super(SomarPaesView, self).get_context_data(**kwargs)
         context["menu"] = menu()
-        context['locais'] = ['Saúde', 'CRAS', 'Reciclagem']
+        context['locais'] = ['Saúde', 'Reciclagem', 'Conselho tutelar', 'CRAS']
         context['totais'] = {'Total':{'peso':0,'quantidade':0}}
         for local in context['locais']:
             context['totais'][local] = {'peso':0,'quantidade':0}
@@ -285,39 +266,118 @@ class SomarPaesView(generic.TemplateView):
         kwargs = context
         return super().get_context_data( **kwargs)
 class ItensEmpresaView(generic.ListView):
-    model = ItemEmpresa
+    
     def lerxlsx(self):
         caminho="itens/static/itens/"
-        nomearq="1-78"
+        nomearq="2-59"
         ext="xlsx"
-        arq = pd.read_excel(f"{caminho}{nomearq}.{ext}",converters={'BARRA':str})
-        dct = arq.to_dict('index')
+        file=f"{caminho}{nomearq}.{ext}"
+        import os
+        print('linha 277')
+        if f'{nomearq}.{ext}' in os.listdir(caminho):
+            print(f'file{file}')
+            arq = pd.read_excel(file,converters={'BARRA':str})
+            dct = arq.to_dict('index')
+        else:
+            dct={}
+            print('linha 284')
         empresa=self.verificaEmpresa( nomearq)
+        #self.cadastraProdutos(empresa,dct)
         return [empresa,dct]
     
     def verificaEmpresa(self, empr):
         empresas = Empresa.objects.filter(cnpj__contains=empr)
+        print(f'linha 292 empresa {empresas}')
         if empresas.count()==1:
             for empresa in empresas:
                 return empresa
-        
-    def cadastraProdutos(self):
-        pass
+
+    def verificaTag(self, *tags):
+        tag = Tag.objects.filter(tipo=tags[0],tag=tags[1]).all()
+        #print(f'tags {tags} tag {tag}')
+        if tag.count()==1:
+            return tag
+        elif tag.count()==0:
+            #print(f'count 0 {tags}')
+            tag = Tag(tipo=tags[0],tag=tags[1],detalhe='ItemEmpresa')
+            tag.save()
+            return self.verificaTag(*tags)
+    def cadastraProdutos(self, empres, itens):
+        titulodados = {
+            'tituloitem':['BARRA','Column1','CODPROD','NCM','COMPOSICAO'],
+            'titulotag':['PERCENTUAL_ICMS','CLASSIFICACAO_PISCOFINS','VENDIDOS','PRECO_CUSTO','PRECO_UNITARIO']
+        }
+        print(f'empresa {empres}\nitens {itens}')
+        for item in itens.values():
+            its = ItemEmpresa.objects.filter(codigo=item['CODPROD'], empresa=empres).all()
+            novoitem = ItemEmpresa(empresa=empres,codigo=item['CODPROD'],descricao=item['Column1'],
+                                barras=item['BARRA'],composicao=item['COMPOSICAO']=="S",ncm=item['NCM'])
+            if its.count()==1:
+                if its[0]['codigo'] == novoitem.codigo:
+                        print (f"it: {its[0]} codigo igual novoitem")
+                        if its[0]['descricao'] != novoitem.descricao:
+                            its[0]['descricao'] = novoitem.descricao
+                            #novoitem.save()
+                            print (f"it: {its[0]} descricao diferente novoitem")
+                        if its[0]['barras'] != novoitem.barras:
+                            its[0]['barras'] = novoitem.barras
+                            #novoitem.save()
+                            print (f"it: {its[0]} barras diferente novoitem")
+                        if its[0]['composicao'] != novoitem.composicao:
+                            its[0]['composicao'] = novoitem.composicao
+                            #novoitem.save()
+                            print (f"it: {its[0]} composicao diferente novoitem")
+                        if its[0]['ncm'] != novoitem.ncm:
+                            its[0]['ncm'] = novoitem.ncm
+                            #novoitem.save()
+                            print (f"it: {its[0]} ncm diferente novoitem")
+                        for ttag in titulodados['titulotag']:
+                            ta = self.verificaTag(ttag,item[ttag])
+                            print (f'item {its[0]} tag {ta} ttag {ttag}:{item[ttag]}')
+                            #ta['tagsitemp'].add(it)
+                            if ta[0] not in its[0].tag.all():
+                                its[0].tag.add(ta[0])    
+                else:
+                    print (f"it: {its[0]} diferente {novoitem}")
+                
+            elif its.count()==0:
+                novoitem.save()
+            else:
+                print(f"its: {its}")
+    def get_queryset(self) -> QuerySet[Any]:
+        self.queryset = ItemEmpresa.objects.all()
+        return super().get_queryset()
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        self.model = ItemEmpresa
+        self.paginate_by = 20
+        #self.ordering = 'descricao'
+        #response = super(ItensEmpresaView, self).get(request)
+        #print(f'response: {request}')
+        return super().get(request, *args, **kwargs)   #response#
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        kwargs = super(ItensEmpresaView, self).get_context_data(**kwargs)
+        #kwargs = super(ItensEmpresaView, self).get_context_data(**kwargs)
         kwargs['menu']=menu() 
-        kwargs['empresa'], kwargs['itens'] = self.lerxlsx()
-        return super().get_context_data(**kwargs)
+        #kwargs['empresa'], kwargs['itens'] = self.lerxlsx()
+        #print (f'linha 360 kwargs {kwargs}')
+        return super().get_context_data(**kwargs)#kwargs#
 class EmpresaView(generic.ListView):
     model = Empresa
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         kwargs = super(EmpresaView, self).get_context_data(**kwargs)
         kwargs['menu']=menu()
-        
+
         return super().get_context_data(**kwargs)
 class TagView(generic.ListView):
     model= Tag
+    paginate_by = 25
+    ordering = 'tipo','tag'
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        #print (f'query 0 tip {queryset[0].tipo}: tag {queryset[0].tag} - det {queryset[0].detalhe} (merc {queryset[0].tagsmerc.all()})')
+        return queryset
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        return super().get(request, *args, **kwargs)
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        kwargs = super(TagView, self).get_context_data(**kwargs)
+        #kwargs = super(TagView,self).get_context_data(**kwargs)
         kwargs['menu']=menu()
         return super().get_context_data(**kwargs)
